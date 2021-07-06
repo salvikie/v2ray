a208d6cd8ba3516d9b08cbb9e7922dcef75d499c
root <root@vultr.guest>
2021-07-06 08:16:51 +0000

    fix ReleaseTag 404 error

--- v2ray-Dockerfile/v2ray.sh
+++ v2ray-Dockerfile/v2ray.sh
@@ -35,6 +35,9 @@ echo "Downloading binary file: ${V2RAY_FILE}"
 echo "Downloading binary file: ${DGST_FILE}"
 
 TAG=$(wget -qO- https://raw.githubusercontent.com/v2fly/docker/master/ReleaseTag | head -n1)
+if [ ${TAG}svk = "svk" ]; then
+	TAG=v4.40.1
+fi
 wget -O ${PWD}/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${V2RAY_FILE} > /dev/null 2>&1
 wget -O ${PWD}/v2ray.zip.dgst https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${DGST_FILE} > /dev/null 2>&1
 
